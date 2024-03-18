63e27583f60f65754f7d4f8a777aaecb5081d4b0
Patrick Chagas Tavares <patrick.c.tavare@gmail.com>
2024-03-17 22:12:37 -0300

    fix: correction scanner

--- pkg/scanner/default.go
+++ pkg/scanner/default.go
@@ -5,7 +5,7 @@ import "fmt"
 type defaultScanner struct{}
 
 func (s defaultScanner) ScanTerminal(format string, value ...any) error {
-	_, err := fmt.Scanf(format, value)
+	_, err := fmt.Scanf(format, value...)
 	return err
 }
 
